b838688e557a20badf4dcfe18acf3ce1da0a6fb5
phancddev <phancddev@gmail.com>
2022-08-08 21:23:58 +0700

    up

--- src/assets general/js/music.js
+++ src/assets general/js/music.js
@@ -1,9 +1,27 @@
-let nhacNen = new Audio('src/sounds_music/solo.mp3')
+let nhacNen = new Audio('/src/sounds _music/solo.mp3')
 
-let music = document.getElementById('music')
-music.addEventListener('click', () => {
-         nhacNen.play()
-         console.log("da chay");
-      })
+// let music = document.getElementById('music')
+// music.addEventListener('click', () => {
+//          nhacNen.play();
+//          console.log("da chay");
+//       })
 
-      
+// let music2 = document.getElementById('bodyMu') 
+
+// music2.addEventListener('click', () => {
+//           check = () => {if(nhacNen.play()) {
+//             return true;
+//          }
+           
+//           }
+//     if(check() == 0) {
+//                nhacNen.pause();
+//             } else
+//    nhacNen.play();
+//             console.log("da chay");
+            
+            
+// })
+$(document).click(function(e) {
+console.log(e);
+ })
