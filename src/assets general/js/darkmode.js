dae4ea48ac3fe4a2c246729bd537dd51f5ca7407
phancddev <phancddev@gmail.com>
2022-08-10 09:16:17 +0700

    dangdev -add

---
+++ src/assets general/js/darkmode.js
@@ -0,0 +1,6 @@
+let layStyleTag = document.getElementById('styleLight');
+
+var phanTuMoi = document.createElement("link");
+    var noiDungMoi
+
+    layStyleTag.setAttribute("href", "./src/assets general/css/darkmode.css")
